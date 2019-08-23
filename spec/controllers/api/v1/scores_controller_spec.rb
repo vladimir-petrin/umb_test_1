b898089f9e38a18d02a6608062162d7f1f73ab35
Vladimir Petrin <horkid@gmail.com>
2019-08-23 16:23:00 +0600

    Contract failure tests fore score creation endpoint

--- spec/controllers/api/v1/scores_controller_spec.rb
+++ spec/controllers/api/v1/scores_controller_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
+RSpec.shared_examples :create_score_contract_failure do |params, expected_errors|
+  let!(:post_instance) { create(:post) }
+
+  it 'does not create post' do
+    expect do
+      post api_v1_post_scores_path(post_instance), params: params
+    end.to_not change { Score.count }.from(0)
+  end
+
+  it 'returns response with 422 status' do
+    post api_v1_post_scores_path(post_instance), params: params
+    expect(response).to have_http_status(422)
+  end
+
+  it 'returns errors' do
+    post api_v1_post_scores_path(post_instance), params: params
+    json = JSON.parse(response.body)
+    expect(json).to match(expected_errors)
+  end
+end
+
 RSpec.describe Api::V1::PostsController, type: :request do
-
-
   describe '#create' do
-    let!(:post_instance) { create(:post) }
-    let!(:score) { create(:score, post: post_instance, value: 4) }
-
     context 'when post exists and value valid' do
+      let!(:post_instance) { create(:post) }
+      let!(:score) { create(:score, post: post_instance, value: 4) }
+
       it 'creates score' do
         expect do
           post api_v1_post_scores_path(post_instance), params: { value: 5 }
@@ -34,7 +53,7 @@ RSpec.describe Api::V1::PostsController, type: :request do
 
     context 'when post does not exist' do
       before do
-        post "/api/v1/posts/#{post_instance.id + 1}/scores", params: { value: 5 }
+        post "/api/v1/posts/999999/scores", params: { value: 5 }
       end
 
       it 'returns response with 404 status' do
@@ -43,17 +62,22 @@ RSpec.describe Api::V1::PostsController, type: :request do
     end
 
     context 'when value invalid' do
-      before do
-        post api_v1_post_scores_path(post_instance), params: { value: 6 }
+      context 'when value is not integer and can not be converted to it' do
+        it_behaves_like :create_score_contract_failure,
+                        { value: { non: :convertable} },
+                        { "value"=>["must be an integer"] }
       end
 
-      it 'returns response with 422 status' do
-        expect(response).to have_http_status(422)
+      context 'when value is less than 1' do
+        it_behaves_like :create_score_contract_failure,
+                        { value: 0 },
+                        { "value"=>["must be greater than or equal to 1"] }
       end
 
-      it 'returns errors' do
-        json = JSON.parse(response.body)
-        expect(json).to eq("value"=>["must be less than or equal to 5"])
+      context 'when value is greater than 5' do
+        it_behaves_like :create_score_contract_failure,
+                        { value: 6 },
+                        { "value"=>["must be less than or equal to 5"] }
       end
     end
   end
